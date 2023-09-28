544eab865eccf42ccc6fe93537d0147f3690cd0c
Peilin <peili@mit.edu>
2023-09-27 21:08:47 -0400

    finish implementing todos

--- server/routes.ts
+++ server/routes.ts
@@ -3,7 +3,7 @@ import { Filter, ObjectId } from "mongodb";
 import { Router, getExpressRouter } from "./framework/router";
 
 import { Post, User, WebSession } from "./app";
-import { PostDoc } from "./concepts/post";
+import { PostAuthorNotMatchError, PostDoc } from "./concepts/post";
 import { UserDoc } from "./concepts/user";
 import { WebSessionDoc } from "./concepts/websession";
 
@@ -64,7 +64,19 @@ class Routes {
   async deletePost(session: WebSessionDoc, _id: ObjectId) {
     // TODO 3: Delete the post with given _id
     // Make sure the user deleting is the author of the post
-    throw new Error("Not implemented!");
+    // const user = await this.getSessionUser(session);
+
+    const user = WebSession.getUser(session);
+    const posts = await Post.read({ _id: _id });
+
+    for (const post of posts) {
+      if (post.author === user) {
+        return await Post.delete(_id);
+      }
+    }
+  
+    throw new PostAuthorNotMatchError(user, _id);    
+    
   }
 }
 
